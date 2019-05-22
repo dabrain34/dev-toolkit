0a64c91e1a6d995186f8786633aab07c5e501302
Stéphane Cerveau <scerveau@collabora.com>
2019-05-22 10:46:30 +0200

    change valgrind supp folder

--- valgrind/valgrind_glib.sh
+++ valgrind/valgrind_glib.sh
@@ -3,8 +3,8 @@ export G_DEBUG=gc-friendly
 export GLIBCPP_FORCE_NEW=1
 export GLIBCXX_FORCE_NEW=1
 
-SUPP="--suppressions=/home/scerveau/DEV/Tools/glibc-2.3.supp
---suppressions=/home/scerveau/DEV/Tools/gst.supp"
+SUPP="--suppressions=glibc-2.3.supp
+--suppressions=gst.supp"
 
 OPTS="-v --trace-children=yes --track-fds=yes --time-stamp=yes --tool=memcheck
 --leak-check=full --leak-resolution=high --freelist-vol=10000000 --show-reachable=yes

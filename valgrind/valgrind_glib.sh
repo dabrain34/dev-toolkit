132075f88b77b3072b380b7043f19f1f00e1ca03
Stéphane Cerveau <scerveau@collabora.com>
2019-10-07 15:54:32 +0200

    valgrind: update scripts to get supp file from folder.

--- valgrind/valgrind_glib.sh
+++ valgrind/valgrind_glib.sh
@@ -3,8 +3,10 @@ export G_DEBUG=gc-friendly
 export GLIBCPP_FORCE_NEW=1
 export GLIBCXX_FORCE_NEW=1
 
-SUPP="--suppressions=glibc-2.3.supp
---suppressions=gst.supp"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+SUPP="--suppressions=$SCRIPT_DIR/glibc-2.3.supp
+--suppressions=$SCRIPT_DIR/gst.supp"
 
 OPTS="-v --trace-children=yes --track-fds=yes --time-stamp=yes --tool=memcheck
 --leak-check=full --leak-resolution=high --freelist-vol=10000000 --show-reachable=yes

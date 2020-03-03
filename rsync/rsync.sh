f1fd6f31b7c6e899664c183d3db9d6bbdfcc262b
Stéphane Cerveau <scerveau@collabora.com>
2020-03-03 21:55:34 +0100

    rsync: split to use only one script for any folder

--- rsync/rsync.sh
+++ rsync/rsync.sh
@@ -4,7 +4,7 @@ usage () {
 
 	echo " Usage: $0 -c <command> -s <source> -d <destination> -e <exclude_file>"
 	echo "     -D or --dry-run      : dry-run"
-  echo "     -c or --command      : backup or restore"
+	echo "     -c or --command      : backup or restore"
 	echo "     -s or --source         : change source folder"
 	echo "     -d or --dest        : vchange dest folder"
 	echo "     -e or --exclude-file      : change exclude file"
@@ -13,16 +13,21 @@ usage () {
 }
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-DEFAULT_BACKUP_FOLDER=/media/$USER/COLLA_CRYPT/BACKUP_XPS
+DEFAULT_BACKUP_FOLDER=/tmp
 EXCLUDE_FILE=$SCRIPT_DIR/rsync-excludes.txt
 SRC=/home/$USER/
-DEST=/media/$USER/COLLA_CRYPT/BACKUP_XPS
 CMD=backup
+DEST=/tmp
 
 while true; do
 	case $1 in
 		-h | --help)
 			usage; exit 0;
+			shift;
+			;;
+        -D | --dry)
+            echo "dry run"
+			DRY=1;
 			shift;
 			;;
 		-c | --command)
@@ -55,4 +60,25 @@ while true; do
 	esac
 done
 
-./rsync.sh -c $CMD -s $SRC -d $DEST -e $EXCLUDE_FILE
+BACKUP_OPTS="-aP"
+RESTORE_OPTS="-rltgoDv"
+
+echo "CMD=$CMD"
+echo "SRC:$SRC"
+echo "DEST=$DEST"
+
+if [ "x$CMD" = "xbackup" ]; then
+RSYNC_CMD="rsync $BACKUP_OPTS --exclude-from=$EXCLUDE_FILE $SRC $DEST"
+elif [ "x$CMD" = "xrestore" ] ; then
+RSYNC_CMD="rsync $RESTORE_OPTS $DEST $SRC"
+else
+echo "RSYNC_CMD: $RSYNC_CMD not found. Exit."
+fi
+
+echo $RSYNC_CMD
+
+if [ ! -z $DRY ]; then
+exit 0
+fi
+
+eval $RSYNC_CMD
